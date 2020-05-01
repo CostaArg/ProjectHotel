2514d841c256ba3e49c79b5262d5c568708770d6
Konstantinos Argyropoulos <konstantinos.argyropoulos@Peoplecert.onmicrosoft.com>
2020-05-01 17:11:03 +0300

    trying to fix slider

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+var output = document.getElementById("currentPrice");
+output.innerHTML = slider.value;
 
-// Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
 } 
