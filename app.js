610e8f0e7ad71980a1f33fabe2947a20850d816e
Konstantinos Argyropoulos <konstantinos.argyropoulos@Peoplecert.onmicrosoft.com>
2020-05-09 22:11:20 +0300

    adding hotels with DOM

--- app.js
+++ app.js
@@ -84,20 +84,137 @@ $(document).ready(function () {
       }
     });
 
-    $(suggestionsPanel).click(function(event) {
+    $(suggestionsPanel).click(function (event) {
       var text = $(event.target).text();
       searchInput.value = text;
-  });
+    });
 
-  //making icons clickable
+    //making icons clickable
 
-  const icon1 = document.querySelector("#icon1");
-  const checkInInput = document.querySelector("#check-in-input");
+    const icon1 = document.querySelector("#icon1");
+    const checkInInput = document.querySelector("#check-in-input");
 
-  $(icon1).click(function() {
-    $(checkInInput).click();
-    console.log("hi");
-});
+    $(icon1).click(function () {
+      $(checkInInput).click();
+      console.log("hi");
+    });
 
+    //adding hotels
+    var loadBtn = document.getElementById("load");
+    loadBtn.addEventListener("click", LoadHotels());
+
+    function LoadHotels() {
+      for (var i in response) {
+        var entries = response[i].entries;
+        for (var j in response[i].entries) {
+          var jsonHotelName = entries[j].hotelName;
+          var jsonStars = entries[j].rating;
+          var jsonCity = entries[j].city;
+          var jsonThumbnail = entries[j].thumbnail;
+          var jsonGuestRating = entries[j].guestrating;
+          var jsonPrice = entries[j].price;
+          var jsonMapUrl = entries[j].mapurl;
+          var jsonRating = entries[j].ratings.no;
+          var jsonRatingText = entries[j].ratings.text;
+
+          var hotelContainer = document.getElementById("hotels");
+
+          var hotelItem = document.createElement("div");
+          hotelItem.setAttribute("class", "hotel-item");
+
+          var hotelImage = document.createElement("img");
+          hotelImage.setAttribute("class", "hotel-image");
+          hotelImage.setAttribute("src", jsonThumbnail);
+          hotelImage.setAttribute("alt", "hotel");
+
+          var hotelDescription = document.createElement("div");
+          hotelDescription.setAttribute("class", "hotel-description-container");
+
+          var hotelTitle = document.createElement("div");
+          hotelTitle.innerHTML = jsonHotelName;
+          hotelTitle.setAttribute("class", "hotel-title");
+
+          var hotelStars = document.createElement("div");
+          hotelStars.innerHTML = jsonStars + " Star Hotel";
+          hotelTitle.setAttribute("class", "hotel-stars");
+
+          var guestRatingContainer = document.createElement("div");
+          hotelTitle.setAttribute("class", "guest-rating-container");
+
+          var guestRating = document.createElement("div");
+          guestRating.setAttribute(
+            "class",
+            "guest-rating smooth-left smooth-right"
+          );
+          guestRating.innerHTML = jsonRating;
+
+          var ratingText = document.createElement("div");
+          ratingText.setAttribute("class", "rating-text");
+          ratingText.innerHTML = jsonRatingText;
+
+          var hotelRating = document.createElement("div");
+          hotelRating.setAttribute("class", "hotel-rating");
+          hotelRating.innerHTML = jsonGuestRating;
+
+          var bar = document.createElement("div");
+          bar.setAttribute("class", "vertical-bar-big");
+
+          var priceSectionContainer = document.createElement("div");
+          priceSectionContainer.setAttribute(
+            "class",
+            "price-section-container"
+          );
+
+          var priceContainer = document.createElement("div");
+          priceContainer.setAttribute("class", "price-container");
+
+          var priceText = document.createElement("div");
+          priceText.setAttribute("class", "price-text");
+          priceText.innerHTML = "Price";
+
+          var price = document.createElement("div");
+          price.setAttribute("class", "price");
+          price.innerHTML = "$" + jsonPrice;
+
+          var priceBtnContainer = document.createElement("div");
+          priceBtnContainer.setAttribute("class", "price-btn-container");
+
+          var priceBtn = document.createElement("div");
+          priceBtn.setAttribute("class", "price-btn smooth-left");
+          priceBtn.innerHTML = "View Deal";
+
+          var arrowRight = document.createElement("span");
+          arrowRight.setAttribute(
+            "class",
+            "material-icons sidearrow-icons smooth-right green-side-arrow"
+          );
+          arrowRight.innerHTML = "keyboard_arrow_right";
+
+          guestRatingContainer.appendChild(guestRating);
+          guestRatingContainer.appendChild(ratingText);
+
+          hotelDescription.appendChild(hotelTitle);
+          hotelDescription.appendChild(hotelStars);
+          hotelDescription.appendChild(guestRatingContainer);
+          hotelDescription.appendChild(hotelRating);
+
+          priceContainer.appendChild(priceText);
+          priceContainer.appendChild(price);
+
+          priceBtnContainer.appendChild(priceBtn);
+          priceBtnContainer.appendChild(arrowRight);
+
+          priceSectionContainer.appendChild(priceContainer);
+          priceSectionContainer.appendChild(priceBtnContainer);
+
+          hotelItem.appendChild(hotelImage);
+          hotelItem.appendChild(hotelDescription);
+          hotelItem.appendChild(bar);
+          hotelItem.appendChild(priceSectionContainer);
+
+          hotelContainer.appendChild(hotelItem);
+        }
+      }
+    }
   }
 });
